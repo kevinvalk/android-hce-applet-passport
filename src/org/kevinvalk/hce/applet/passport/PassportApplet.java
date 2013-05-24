7c276839a4d9c90813b2cc338cdcd4b8bd196b93
Kevin Valk <kevin@kevinvalk.nl>
2013-05-24 20:11:44 +0200

    Implemented change by 0bd49f3, correctly closing thread

--- src/org/kevinvalk/hce/applet/passport/PassportApplet.java
+++ src/org/kevinvalk/hce/applet/passport/PassportApplet.java
@@ -4,10 +4,19 @@ import org.kevinvalk.hce.framework.Apdu;
 import org.kevinvalk.hce.framework.Applet;
 import org.kevinvalk.hce.framework.Iso7816;
 
+import android.util.Log;
+
 public class PassportApplet extends Applet
 {
 	private Passport passport;
+	private static final String APPLET_NAME = "e-passport";
 	private static final byte[] APPLET_AID = { (byte) 0xA0, 0x00, 0x00, 0x02, 0x47, 0x10, 0x01 };
+
+	
+	static public void d(String msg)
+	{
+		Log.d("EPASS", msg);
+	}
 	
 	public PassportApplet(Passport passport)
 	{
@@ -19,31 +28,42 @@ public class PassportApplet extends Applet
 	{
 		// We are running and we can send we are OK
 		isRunning = true;
-		Apdu apdu = sendApdu(new Apdu(Iso7816.SW_NO_ERROR));
 		
 		// Lets start handling all incomming traffic
 		do
 		{
 			try
 			{
-				handleApdu(apdu);
+				Apdu response = handleApdu(apdu);
+				
+				
+				// Check if we have response left
+				if (response != null)
+					apdu = sendApdu(response);
+				else
+					isRunning = false;
 			}
 			catch(Exception e)
 			{
-				
+			
+				isRunning = false;
 			}
-			
-			
 		}
 		while(isRunning && apdu != null);
-		// TODO Auto-generated method stub
-		
+		d("Stopping");
 	}
 
 	@Override
-	public boolean handleApdu(Apdu apdu) {
-		// TODO Auto-generated method stub
-		return false;
+	public Apdu handleApdu(Apdu apdu)
+	{
+		d("Handle apdu");
+		return null;
+	}
+	
+	@Override
+	public String getName()
+	{
+		return APPLET_NAME;
 	}
 
 	@Override
